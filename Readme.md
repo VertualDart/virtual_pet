605857aba4f4f3ea6787da86522a6031b23e6cd7
CodingBicep <dart010904@gmail.com>
2025-07-17 22:06:09 +0530

    Seconds out

---
+++ Readme.md
@@ -0,0 +1,7 @@
+as of now i only got it to show an image on the desktop (on top of everything)
+
+plan to go: 
+1. make it draggable
+2. make it responsive
+3. make it animated
+4. make it better
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import tkinter as tk
+import tkinter as tk 
 from PIL import Image, ImageTk
 
-class VirtPet:
+class VirtuPet():
     def __init__(self):
         self.root = tk.Tk()
         self.root.overrideredirect(True)
@@ -16,9 +16,24 @@ class VirtPet:
 
         self.root.geometry(f"-100-100")
 
+        self.canvas.bind("<ButtonPress-1>", self.start_move)
+        self.canvas.bind("<B1-Motion>", self.do_move)
+
+    def start_move(self, event):
+        self.x = event.x
+        self.y = event.y
+    
+    def do_move(self, event):
+        deltax = event.x - self.x
+        deltay = event.x - self.y
+        x = self.root.winfo_x() + deltax
+        y = self.root.winfo_y() + deltay
+        self.root.geometry(f"+{x}+{y}")
+
+
     def run(self):
         self.root.mainloop()
 
 if __name__ == "__main__":
-    pet = VirtPet()
+    pet = VirtuPet()
     pet.run()
